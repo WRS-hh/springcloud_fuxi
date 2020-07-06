2fccb7c8d63441d1091ab8e80ff940a3331655c9
wangrushui <wangrushui@qq.com>
2020-07-06 11:03:58 +0800

    注册中心服务搭建

--- springcloud_parent/springcloud_order/src/main/java/com/itcast/cloud/OrderApplication.java
+++ springcloud_parent/springcloud_order/src/main/java/com/itcast/cloud/OrderApplication.java
@@ -3,9 +3,11 @@ package com.itcast.cloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.itcast.cloud.mapper")
+@EnableDiscoveryClient
 public class OrderApplication {
 
     public static void main(String[] args) {
